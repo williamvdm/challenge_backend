7368afd27d0360327cef38471dd96b092d67c70c
Will <williamvdm@live.nl>
2023-04-12 14:18:52 +0200

    add: comments

--- src/Controller/ParcelController.php
+++ src/Controller/ParcelController.php
@@ -10,16 +10,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// Prefix voor alle routes in de controller
 /**
  * @Route("/", name="api_")
  */
+
+// Definieert naam van de controller en extend de AbstractController symfony class.
 class ParcelController extends AbstractController
 {
+    // Behandelt de POST voor een pakket.
+    // De @Route("/parcels" is de route van deze endpoint en is toegankelijk via de POST request in de front end.
+    // ManagerRegistry $doctrine zorgt ervoor dat er toegang is tot de database. Request $request bevat de informatie van de request die de user verstuurd.
     /**
      * @Route("/parcels", name="post_parcels", methods={"POST"})
      */
     public function index(ManagerRegistry $doctrine, Request $request): Response
     {
+        // Deze regels halen de waardes uit het Request-object (de gegevens die in de front-end zijn ingevoerd) die nodig zijn om een parcel aan te maken.
         $entityManager = $doctrine->getManager();
         $name = $request->request->get('name');
         $deliveryDate = DateTime::createFromFormat('Y-m-d', $request->request->get('deliveryDate'));
@@ -31,9 +38,12 @@ class ParcelController extends AbstractController
         $length = $request->request->get('length');
         $height = $request->request->get('height');
         $width = $request->request->get('width');
+
+        // Het tracking nummer wordt gegenereerd d.m.v. base64 encoding. Dit zorgt ervoor dat
         $tracking = substr(base64_encode($name . $request->request->get('deliveryDate') . $address . $postcode . $city . $country . $weight . $height . $width), 0, 8);
         $status = "Aangemeld";
 
+        //Hier wordt een nieuw Parcel aangemaakt met de gegevens uit de POST request.
         $parcel = new Parcel();
         $parcel->setName($name);
         $parcel->setDeliveryDate($deliveryDate);
@@ -62,8 +72,7 @@ class ParcelController extends AbstractController
         $parcel = $doctrine->getRepository(Parcel::class)->findOneBy(["trackingNumber"=>$trackingNumber]);
 
         if (!$parcel) {
-
-            return $this->json("Geen parcel gevonden met trackingnummer " . $trackingNumber, 404);
+            return $this->json(['error' => "Geen parcel gevonden met trackingnummer " . $trackingNumber], 404);
         }
         $data =  [
             'name' => $parcel->getName(),
@@ -78,7 +87,7 @@ class ParcelController extends AbstractController
                 'width' => $parcel->getWidth()
             ],
             'trackingNumber' => $parcel->getTrackingNumber(),
-            'status' => $parcel->getParcelStatus()
+            'status' => $parcel->getParcelStatus(),
         ];
 
         return $this->json($data);
